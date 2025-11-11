2d6750ab9566bcf689897511be7c7be7207a2887
Fadilah Abdulkadir <fadeelzy@gmail.com>
2025-11-11 09:18:04 +0100

    Fix DB SSL + static file

--- devproject/settings.py
+++ devproject/settings.py
@@ -87,10 +87,15 @@ import dj_database_url
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,   
-        ssl_require=True    
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
     )
+}
+
+DATABASES["default"]["OPTIONS"] = {
+    "ssl": {
+        "cert_reqs": 0  
+    }
 }
 
 DEBUG = os.getenv('DEBUG') == 'True'
